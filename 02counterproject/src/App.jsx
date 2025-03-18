11e584a522cda0b9960ddd16b44b953a387f98f0
Parth Tiwari <parthtiwaristjoseph@gmail.com>
2025-03-19 02:25:07 +0530

    project in app.jsx

--- 02counterproject/src/App.jsx
+++ 02counterproject/src/App.jsx
@@ -5,9 +5,39 @@ import './App.css'
 
 function App() {
 
+  let [counter , setCounter] = useState(0)
+
+  //let counter=15
+
+  const addvalue=()=>{
+    if(counter===20){
+      setCounter(counter=20)
+    }else{
+    
+    //counter=counter+1
+    setCounter(counter+1)
+    }
+  }
+  const subValue=()=>{
+    if(counter===0){
+      setCounter(counter=0)
+    }else{
+    
+    //counter=counter-1
+    setCounter(counter-1)
+    }
+  }
+
   return (
     <>
       <h1>Chai aur React</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button
+      onClick={addvalue}>Add Value</button>
+      <br></br>
+      <button 
+      onClick={subValue}>Subtract Value</button>
     </>
   )
 }
