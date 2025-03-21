2a09469dd404fbf1eb8687609a8dcefb768da857
Parth Tiwari <parthtiwaristjoseph@gmail.com>
2025-03-21 22:22:16 +0530

    fix indentation in App component

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -5,8 +5,8 @@ import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
 
-  const [amount, setAmount] = useState(0)
-  const [from, setFrom] = useState("usd")
+    const [amount, setAmount] = useState(0)
+    const [from, setFrom] = useState("usd")
   const [to, setTo] = useState("inr")
   const [convertedAmount, setConvertedAmount] = useState(0)
 
