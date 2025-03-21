72a9f2fb5e87c68fcef8c1a644a385df1dead40b
Parth Tiwari <parthtiwaristjoseph@gmail.com>
2025-03-21 22:17:46 +0530

    initialize currency converter app with React, Vite, and Tailwind CSS setup

---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import {useEffect, useState} from "react"
+
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@{2025-03-01}/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
+}
+
+export default useCurrencyInfo;
+//+ https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/{endpoint}
