2ffc9456dad0f77d318d43aba9a439f228815d54
Parth Tiwari <parthtiwari670@gmail.com>
2025-03-22 14:06:25 +0530

    feat: initialize React app with Vite, add user context and basic components

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,7 @@
+// context ek major component hai jo ki hume multiple component me data pass karne me help karta hai.
+import React from 'react'
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
