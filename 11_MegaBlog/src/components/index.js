6fe55432de59fc0248186847bf62c37353c3cb93
Parth Tiwari <parthtiwari670@gmail.com>
2025-04-01 20:43:46 +0530

    feat: add reusable components including Button, Input, Logo, Container, and Logout button; enhance Header and Footer with navigation and layout

--- 11_MegaBlog/src/components/index.js
+++ 11_MegaBlog/src/components/index.js
@@ -1,6 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export{
-    Header,Footer
+    Header,Footer,Container,LogoutBtn
 }
