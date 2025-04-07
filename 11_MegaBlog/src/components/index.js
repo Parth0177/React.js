10d44233ebaf02f9061e45181847c241a3e29087
Parth Tiwari <parthtiwari670@gmail.com>
2025-04-07 18:17:14 +0530

    feat: add Login component for user authentication with form handling and error management

--- 11_MegaBlog/src/components/index.js
+++ 11_MegaBlog/src/components/index.js
@@ -2,7 +2,10 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Input from "./Input";
+import Button from "./Button";
 
 export{
-    Header,Footer,Container,LogoutBtn
+    Header,Footer,Container,LogoutBtn,Logo,Input,Button
 }
